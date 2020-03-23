135b6df9e72266ee2c9aa82d4b289f91d0078239
wrecks <rhaber@outlook.com>
2020-03-22 20:44:59 -0700

    Update button_example.py

--- main/button_example.py
+++ main/button_example.py
@@ -10,7 +10,7 @@ button = Pin(15, Pin.IN)
 
 while True:
   button_state = button.value()
-  buzz=PWM(Pin(32), freq=440)
+  buzz=PWM(Pin(32), freq=1000)
   buzz.duty(1012) if button_state==1 else buzz.duty(0)
 
       
