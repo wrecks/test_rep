08050e259ba0ba02539bd0779f5753f35dc55c59
wrecks <rhaber@outlook.com>
2020-03-22 20:31:59 -0700

    Add files via upload

---
+++ main/button_example.py
@@ -0,0 +1,20 @@
+"""button test"""
+
+from machine import Pin, PWM
+from time import sleep
+
+led = Pin(32, Pin.OUT)
+button = Pin(15, Pin.IN)
+
+
+
+while True:
+  button_state = button.value()
+  buzz=PWM(Pin(32), freq=440)
+  buzz.duty(512) if button_state==1 else buzz.duty(0)
+
+      
+      #led.value(button_state)
+    
+
+  #sleep(0.1)
